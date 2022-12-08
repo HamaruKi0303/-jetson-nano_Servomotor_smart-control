42e1068c66304a133dc4aab0cd1422f03521e5da
Maki <haru.maki.ch.0303@gmail.com>
2022-12-08 20:13:55 +0900

    add form

--- apps/home/servomotor_smart_control.py
+++ apps/home/servomotor_smart_control.py
@@ -2,8 +2,13 @@ from flask import Flask, render_template, url_for, request, redirect, Blueprint
 from datetime import datetime
 
 import pandas as pd
+import os
 import pprint
 from loguru import logger
+from datetime import datetime as dt
+
+
+
 # Blueprint を作成
 bp = Blueprint('servomotor_smart_control', __name__)
 
@@ -15,17 +20,28 @@ def servomotor_smart_control():
     # running_type = "develop"
     running_type = "master"
     
-    form_data_path = "apps/static/assets/data/form_data.csv"
-    df_form = pd.read_csv(form_data_path, index_col=0)
+    tdatetime = dt.now()
+    tstr = tdatetime.strftime('%Y%m%d_%H%M%S')
     
+    data_path = "apps/static/assets/data/smart_control_data.csv"
+    is_file = os.path.isfile(data_path)
+    if(is_file):
+        df_form = pd.read_csv(data_path, index_col=0)
+    else:
+        df_form = pd.DataFrame({})
     # POSTメソッドの場合
     if request.method == 'POST':
 
         dict_form = request.form.to_dict()
-        df_form = df_form.append(dict_form, ignore_index=True)
+        dict_form["date"] = tstr
+        if(is_file):
+            df_form = df_form.append(dict_form, ignore_index=True)
+        else:
+            # pprint.pprint(dict_form)
+            df_form = pd.DataFrame(dict_form, index=[0])
         
         logger.info("df_form")
-        df_form.to_csv(form_data_path)
+        df_form.to_csv(data_path)
         pprint.pprint(df_form)
         
     logger.info("dict_list_form")
@@ -33,7 +49,7 @@ def servomotor_smart_control():
     pprint.pprint(dict_list_form)
     # name = request.form['name']
     
-    return render_template('sample/servomotor_smart_control.html', 
+    return render_template('smart_control/servomotor_smart_control.html', 
                             dict_list_form=dict_list_form, 
                             segment=segment, 
                             running_type=running_type)
